20cab31bac5b8c68286e5b9db6573b348f5b3f43
Aytan <a.gurbanova12@gmail.com>
2025-06-15 02:28:18 +0300

    project done

---
+++ index.js
@@ -0,0 +1,31 @@
+let homeScore = document.getElementById("home-score-value");
+let guestScore = document.getElementById("guest-score-value");
+function add1home() {
+    let currentScore = parseInt(homeScore.textContent, 10);
+    homeScore.textContent = currentScore + 1;
+}
+
+function add2home() {
+    let currentScore = parseInt(homeScore.textContent, 10);
+    homeScore.textContent = currentScore + 2
+}
+
+function add3home() {
+    let currentScore = parseInt(homeScore.textContent, 10);
+    homeScore.textContent = currentScore + 3
+}
+
+function add1guest() {
+    let currentScore = parseInt(guestScore.textContent, 10);
+    guestScore.textContent = currentScore + 1
+}
+
+function add2guest() {
+    let currentScore = parseInt(guestScore.textContent, 10);
+    guestScore.textContent = currentScore + 2
+}
+
+function add3guest() {
+    let currentScore = parseInt(guestScore.textContent, 10);
+    guestScore.textContent = currentScore + 3
+}
